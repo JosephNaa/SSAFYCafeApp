a3b73435fd5f5b68f9dae1662f416c103a55820c
JosephNaa <js.pekah@gmail.com>
2021-11-22 10:21:38 +0900

    [feat] splash activity 적용

---
+++ app/app/src/main/java/com/ssafy/smartstoredb/ui/splash/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.ssafy.smartstoredb.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.ssafy.smartstoredb.databinding.ActivitySplashBinding
+import com.ssafy.smartstoredb.ui.login.LoginActivity
+
+class SplashActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, 1500)
+    }
+}
