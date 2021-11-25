8c8c5161e75fa35d1af78fbde19b237826e39ecc
최현정 <owo339@naver.com>
2021-11-25 20:52:56 +0900

    [refactor] ProducApi 주석 정리

--- app/app/src/main/java/com/ssafy/smartstoredb/data/remote/api/ProductApi.kt
+++ app/app/src/main/java/com/ssafy/smartstoredb/data/remote/api/ProductApi.kt
@@ -10,12 +10,10 @@ import retrofit2.http.Path
 interface ProductApi {
     // 전체 상품의 목록을 반환한다
     @GET("rest/product")
-    // fun getProductList(): Call<List<Product>>
     suspend fun getProductList(): Response<List<Product>>
 
     // {productId}에 해당하는 상품의 정보를 comment와 함께 반환한다.
     // comment 조회시 사용
     @GET("rest/product/{productId}")
-    // fun getProductWithComments(@Path("productId") productId: Int): Call<List<MenuDetailWithCommentResponse>>
     suspend fun getProductWithComments(@Path("productId") productId: Int): Response<List<MenuDetailWithCommentResponse>>
 }
