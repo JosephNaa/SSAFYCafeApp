ccc322e6035a745e3f19523e9b44375c71eb4920
최현정 <owo339@naver.com>
2021-11-25 17:09:02 +0900

    [refactor] rest/product를 Corouintes로 리팩토링

--- app/app/src/main/java/com/ssafy/smartstoredb/data/remote/api/ProductApi.kt
+++ app/app/src/main/java/com/ssafy/smartstoredb/data/remote/api/ProductApi.kt
@@ -3,13 +3,15 @@ package com.ssafy.smartstoredb.data.remote.api
 import com.ssafy.smartstore.response.MenuDetailWithCommentResponse
 import com.ssafy.smartstoredb.model.dto.Product
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ProductApi {
     // 전체 상품의 목록을 반환한다
     @GET("rest/product")
-    fun getProductList(): Call<List<Product>>
+    // fun getProductList(): Call<List<Product>>
+    suspend fun getProductList(): Response<List<Product>>
 
     // {productId}에 해당하는 상품의 정보를 comment와 함께 반환한다.
     // comment 조회시 사용
